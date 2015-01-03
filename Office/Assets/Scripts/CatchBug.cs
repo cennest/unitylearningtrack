9e5c83d61e1b2168b098a782fee037f7f0b46a3e
Pradeep <pradeep.vishwakarma@cennest.com>
2015-01-03 15:28:50 +0530

    sound effect added

---
+++ Office/Assets/Scripts/CatchBug.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class CatchBug : MonoBehaviour
+{
+		public AnimationClip deathAnimation;
+	    public AudioClip deathClip;
+		public AIFollow aiFollow;
+	    public UILabel developerBugCountLabel;
+	    public UILabel testerBugCountLabel;
+
+		void OnTriggerEnter (Collider other)
+		{
+				if (other.gameObject.tag == "Developer") {
+						animation.CrossFade (deathAnimation.name);
+						aiFollow.canMove = false;
+						audio.PlayOneShot (deathClip);
+						Destroy (this.gameObject, 1);
+
+						int count = 0;
+						System.Int32.TryParse (developerBugCountLabel.text, out count);
+						count++;
+
+						developerBugCountLabel.text = count.ToString ();
+
+				} else if (other.gameObject.tag == @"Tester") {
+				
+			            audio.PlayOneShot (deathClip);
+						Destroy (this.gameObject);
+						int count = 0;
+						System.Int32.TryParse (testerBugCountLabel.text, out count);
+						count++;
+						testerBugCountLabel.text = count.ToString ();
+				}
+		}
+}
