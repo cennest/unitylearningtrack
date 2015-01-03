dde903773a1a72c686513357a4f1c1a38314be49
Pradeep <pradeep.vishwakarma@cennest.com>
2015-01-03 13:24:49 +0530

    multilple bug created

---
+++ Office/Assets/TesterCatch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TesterCatch : MonoBehaviour {
+
+	public AIFollow aiFollow;
+	public GameObject bug;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Transform[] ts = bug.GetComponentsInChildren<Transform>();
+
+		foreach (Transform t in ts) {
+						if (t != null && t.gameObject.activeInHierarchy)
+								aiFollow.target = t;		
+				}
+	}
+}
