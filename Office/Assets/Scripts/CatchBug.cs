5a2372894d85b25cbf68ee853bb0a61dc8abe1d0
Pradeep <pradeep.vishwakarma@cennest.com>
2015-01-03 16:30:13 +0530

    collision issue fixed + jump animation added for tester pickup

--- Office/Assets/Scripts/CatchBug.cs
+++ Office/Assets/Scripts/CatchBug.cs
@@ -6,33 +6,45 @@ using System.Collections;
 public class CatchBug : MonoBehaviour
 {
 		public AnimationClip deathAnimation;
+	    public AnimationClip jumpAnimation;
 	    public AudioClip deathClip;
+	    public AudioClip jumpClip;
 		public AIFollow aiFollow;
 	    public UILabel developerBugCountLabel;
 	    public UILabel testerBugCountLabel;
-
+	   
 		void OnTriggerEnter (Collider other)
 		{
 				if (other.gameObject.tag == "Developer") {
+						removeCollider ();
 						animation.CrossFade (deathAnimation.name);
 						aiFollow.canMove = false;
-						audio.PlayOneShot (deathClip);
-						Destroy (this.gameObject, 1);
-
+						audio.PlayOneShot (deathClip);						
 						int count = 0;
 						System.Int32.TryParse (developerBugCountLabel.text, out count);
 						count++;
-
 						developerBugCountLabel.text = count.ToString ();
+						Destroy (this.gameObject, 1);
 
 				} else if (other.gameObject.tag == @"Tester") {
-				
-			            audio.PlayOneShot (deathClip);
-						Destroy (this.gameObject);
+						removeCollider ();
+						animation.CrossFade (jumpAnimation.name);
+						aiFollow.canMove = false;
+						audio.PlayOneShot (jumpClip);						
 						int count = 0;
 						System.Int32.TryParse (testerBugCountLabel.text, out count);
 						count++;
 						testerBugCountLabel.text = count.ToString ();
+						Destroy (this.gameObject, 1);
 				}
 		}
+
+	void removeCollider ()
+	{
+		BoxCollider[] myColliders = gameObject.GetComponents<BoxCollider>();
+		foreach(BoxCollider bc in myColliders) {
+			bc.enabled = false;
+		}
+	}
+
 }
