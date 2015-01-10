15c7eef479837317e8b59a7c88f2a1f53f806c76
Pradeep <pradeep.vishwakarma@cennest.com>
2015-01-10 18:11:01 +0530

    axe and axe animation added

---
+++ Office/Assets/Scripts/AttackAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackAnimation : MonoBehaviour {
+
+	public AnimationClip AttackAnimationClip;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButton (0)) {
+						animation.Play (AttackAnimationClip.name);
+				} else if (animation [AttackAnimationClip.name].time > animation [AttackAnimationClip.name].length) {
+			animation.CrossFade(animation.clip.name);		
+		}
+
+	}
+}
