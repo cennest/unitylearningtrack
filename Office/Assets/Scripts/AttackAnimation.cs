b9d1495f730bde01583216583a8709d3da5bad68
Stalin Jose <stalin.jose@cennest.com>
2015-02-28 16:25:37 +0530

    item swap logic added

--- Office/Assets/Scripts/AttackAnimation.cs
+++ Office/Assets/Scripts/AttackAnimation.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AttackAnimation : MonoBehaviour {
+public class AttackAnimation : MonoBehaviour,IPickupable {
 
 	public AnimationClip AttackAnimationClip;
+	bool isMounted = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,9 @@ public class AttackAnimation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if (!isMounted)
+						return;
+
 		if (Input.GetMouseButton (0)) {
 						animation.Play (AttackAnimationClip.name);
 				} else if (animation [AttackAnimationClip.name].time > animation [AttackAnimationClip.name].length) {
@@ -20,4 +24,14 @@ public class AttackAnimation : MonoBehaviour {
 		}
 
 	}
+
+	#region IPickupable implementation
+
+	public void PickUp (GameObject player)
+	{
+		isMounted = true;
+		animation.Play ();
+	}
+
+	#endregion
 }
---
+++ Office/Assets/Scripts/ItemSwap.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSwap : MonoBehaviour {
+
+	Transform mountPoint;
+	public Transform StashPoint;
+
+	// Use this for initialization
+	void Start () {
+		mountPoint = GetComponent<PlayerPickUp> ().mountPoint;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+
+			var mountedItem = GetItem(mountPoint);
+			var stasheditem = GetItem(StashPoint);
+
+			Move (mountedItem,StashPoint);
+			Move (stasheditem,mountPoint);
+		}
+
+	}
+
+	private Component GetItem(Transform point) {
+		return point.GetComponentInChildren(typeof(IPickupable));
+	}
+
+	private void Move (Component mountedPoint, Transform to) {
+		if (mountedPoint != null) {
+						mountedPoint.transform.parent = to;
+				}
+	}
+
+}
